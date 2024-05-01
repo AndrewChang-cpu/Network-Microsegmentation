a29e6d6e0449df9e1f07966d459cf8c83c9ba769
Adi Pillai <76452932+Adi9103@users.noreply.github.com>
2024-05-01 18:43:48 -0500

    updated for cors

--- controller/server.js
+++ controller/server.js
@@ -1,21 +1,26 @@
-// Import Express and other required modules
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const policyRoutes = require('./policyRoutes');
 
-// Create an Express application
+
 const app = express();
 
-// Use body-parser middleware to parse JSON requests
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}));
 app.use(bodyParser.json());
 
-// Use the imported routes for the /api/policies path
+
 app.use('/api', policyRoutes);
 
-// Set the port for the application
+
 const PORT = process.env.PORT || 3000;
 
-// Make the application listen on the specified port
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
